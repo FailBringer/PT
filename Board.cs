251292b8c7226bf0b1df024119e2e2919e02dada
FailBringer <fordocs1234@gmail.com>
2016-06-05 20:29:36 +0400

    The First Commit.

---
+++ Board.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	// Use this for initialization
+	public class Map {
+		public int[] avanguard = new int[3];
+		public int[] flank = new int[2];
+		public int Leader;
+		public int[] rear = new int[3];
+	}
+
+	private Map myMap;
+	private Map enemyMap;
+
+	void Start () {
+		myMap = new Map ();
+		enemyMap = new Map();
+		myMap.avanguard [2] = 1;
+		Debug.Log(myMap.avanguard[2].ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Cards.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Cards : MonoBehaviour {
+
+
+	private List<int> cards = new List<int>();
+	private List<int> cardsOnHand = new List<int>();
+
+	// Use this for initialization
+	void Start () {
+		cards = new Shuffle ().GetDeck ();
+		int startCardCount = 0;
+		while (startCardCount < 5) {
+			cardsOnHand.Add(cards[0]);
+			cards.RemoveAt(0);
+			startCardCount++;
+		}
+		Test ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void Test(){
+		string testStr = "";
+		foreach (int con in cardsOnHand) {
+			testStr += con.ToString() + " ";
+		}
+		Debug.Log (testStr);
+
+	}
+}
